a206202ac989d17c4c0b88fd0e105919d1c784b1
Jeffrey Li <jeff@jeffreys-air-4.wifi.local.cmu.edu>
2023-09-05 17:03:50 -0400

    improving design and encapsulation for java files

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rect = new Rectangle(7, 3);
+        Shape circ = new Circle(1.0);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(rect);
+        Renderer renderer2 = new Renderer(circ);
 
         renderer.draw();
+        renderer2.draw();
     }
 }
